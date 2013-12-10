b69e4ab6da717709b6073941106ea5485c94e286
Warren Seymour <wms@fountainhead-solutions.net>
2013-12-10 16:43:28 +0000

    Angular refactor
    Load angular from CDN
    Remove jquery dependency
    Remove un-needed controller bootstrapping
    Replace nested callbacks with promise chaining

--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,22 @@
-var app = angular.module('unifiedDiffApp', []);
+angular.module('unifiedDiffApp', [])
+.run(function($window) {
 
-app.config(function($interpolateProvider) {
+    // send user to video for old hash-style links
+    var video = $window.location.hash;
+    if (video) {
+        window.location.href = 'videos.html' + video;
+    }
+
+}).config(function($interpolateProvider, $httpProvider) {
+
     $interpolateProvider.startSymbol('{[{');
     $interpolateProvider.endSymbol('}]}');
-});
 
-app.controller('NextEventCtrl', function ($scope, $http) {
+    // Disable OPTIONS preflight CORS request
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+}).controller('NextEventCtrl', function ($scope, $http) {
+
     angular.extend($scope, {
         events: [],
         rsvps: [],
@@ -16,41 +27,33 @@ app.controller('NextEventCtrl', function ($scope, $http) {
             }
 
             return "https://pbs.twimg.com/profile_images/1777603509/logo-128px_bigger.png";
+        },
+
+        loadEvents: function() {
+            return $http.get('http://unified-diff.marvelley.com/events.json')
+                .success(function(data) {
+                    $scope.events = data;
+                });
+        },
+
+        loadRsvps: function(response) {
+            var events = response.data;
+
+            if (events.length) {
+                $http.get('http://unified-diff.marvelley.com/rsvps/' + events[0].id + '.json')
+                .success(function(data) {
+                    $scope.rsvps = _.filter(data, function(user) {
+                        return user.response === 'yes';
+                    });
+                });
+            }
+        },
+
+        init: function() {
+            $scope.loadEvents().then($scope.loadRsvps);
         }
     });
 
-    $scope.$on('$viewContentLoaded', $scope.init);
+    $scope.init();
 
-    $scope.init = function () {
-        $scope.loadNextEvent();
-    };
-
-    $scope.loadNextEvent = function () {
-        $.ajax({
-            url: 'http://unified-diff.marvelley.com/events.json',
-            success: function (data) {
-                $scope.$apply(function () {
-                    $scope.events = data;
-
-                    if (data.length) {
-                        $scope.loadRsvps(data[0]);
-                    }
-                });
-            }
-        })
-    };
-
-    $scope.loadRsvps = function (event) {
-        $.ajax({
-            url: 'http://unified-diff.marvelley.com/rsvps/' + event.id + '.json',
-            success: function (data) {
-                $scope.$apply(function () {
-                    var rsvps = _.filter(data, function(user){ 
-                        return user.response === 'yes'; 
-                    });
-                    $scope.rsvps = rsvps;
-                });
-            }
-        })
-    };
 });
